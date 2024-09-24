23cdff280f2c6d0a819ff88064bc44a4dbf5ec11
Nhan Nguyen <nhan.nguyen1624@gmail.com>
2024-09-24 00:38:38 -0700

    adding live video feed

--- video/vid.py
+++ video/vid.py
@@ -3,19 +3,19 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 
 
 def generate_frames():
     while True:
-        sucess, frame = camera.read()
+        success, frame = camera.read()
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', processed_frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
 
-            yield (b'--fram\r\n'
+            yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
